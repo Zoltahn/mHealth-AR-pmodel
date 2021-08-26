687dcf234be313fe2e6130d7574f70936ad23985
Zoltahn <zoltahn@gmail.com>
2021-08-26 14:31:52 +1000

    Setting up file structure

---
+++ dataprocess.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 26 14:15:37 2021
+
+@author: zolta
+"""
+
+import tensorflow as tf
+import numpy as npy
+import pandas as pnd
+
+def loadHARSet(filepath = ''):
+    a=0
+    
+    return a
+
+def loadWISMDset(filepath = ''):
+    a=0
+    
+    return a
--- model.py
+++ model.py
@@ -6,3 +6,16 @@ Created on Tue Aug 24 15:41:23 2021
 """
 
 import tensorflow as tf
+from tensorflow import keras as ke
+from tensorflow.keras import layers
+
+def CNNModel():
+    model = ke.models.Sequential()
+    
+    return model
+
+
+def RNNModel():
+    model = ke.models.Sequential()
+    
+    return model
