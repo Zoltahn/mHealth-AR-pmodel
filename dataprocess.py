bc60b04e4d168e2e84cc1f2a944e38f3d8270132
Zoltahn <zoltahn@gmail.com>
2021-09-01 14:20:10 +1000

    ignore file

--- dataprocess.py
+++ dataprocess.py
@@ -14,7 +14,13 @@ def loadHARSet(filepath = ''):
     
     return a
 
-def loadWISMDset(filepath = ''):
+def loadWISMDset(filepath = '', option =1):
     a=0
+    if(option == 2):
+        dSet = 'WISDM_ar_v1.1'
+    else:
+        dSet = 'WISDM_at_v2.0'
     
+    
+        
     return a
